ba20f5dd3ce1a992ef526225c4422157e4ad77cc
cynthicrush <cynthiazcanada@gmail.com>
2021-02-22 22:52:51 -0500

    Cynthia-Canada

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,16 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
+
+import { Route, Link, Switch, useHistory } from 'react-router-dom';
+import MovieCard from './Movies/MovieCard';
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
+
 
   useEffect(() => {
     const getMovies = () => {
@@ -14,14 +20,15 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          setMovieList(response.data);
         })
         .catch(error => {
           console.error('Server Error', error);
         });
-    }
+    };
     getMovies();
   }, []);
-
+  console.log('movieList', movieList);
   const addToSavedList = id => {
     // This is stretch. Prevent the same movie from being "saved" more than once
   };
@@ -29,8 +36,14 @@ export default function App () {
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
+      <Route path='/movies/:id'>
+        <Movie movies={movieList} />
+      </Route>
+      <Route exact path='/' >
+        <MovieList movies={movieList} />
+      </Route>
+      {/* <Route path='/movies/:id' render={(route) => <Movie id={route.match.params.id} /> } /> */}
 
-      <div>Replace this Div with your Routes</div>
     </div>
   );
 }
